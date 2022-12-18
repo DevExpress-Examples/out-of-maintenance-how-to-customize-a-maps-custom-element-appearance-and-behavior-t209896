44006156c532229bec40c3d2458522540c038d05
DevExpressExampleBot <fakeEmail>
2022-12-18 17:42:40 +0400

    Source auto update [skip ci]

--- VB/MapCustomElement/MainWindow.xaml.vb
+++ VB/MapCustomElement/MainWindow.xaml.vb
@@ -1,19 +1,24 @@
-﻿Imports System
+Imports System
 Imports System.Windows
 
 Namespace MapCustomElement
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    Public Partial Class MainWindow
+        Inherits Window
 
-	Friend Class ShipwreckInformation
-		Public Property ImageUri() As Uri
-		Public Property Name() As String
-		Public Property Year() As Int32
-		Public Property Description() As String
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
+
+    Friend Class ShipwreckInformation
+
+        Public Property ImageUri As Uri
+
+        Public Property Name As String
+
+        Public Property Year As Integer
+
+        Public Property Description As String
+    End Class
 End Namespace
